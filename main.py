ffa96160f4cc89ab958a60397ffc63018c7d500b
nFinityV2 <nFinitySoh@hotmail.com>
2025-04-24 15:14:29 +1000

    Added and draw player sprites

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, PLAYER_RADIUS)
     
     clock = pygame.time.Clock()
     dt = 0
@@ -16,6 +18,7 @@ def main():
         
         pygame.Surface.fill(screen, "black")
         dt = clock.tick(60) / 1000
+        player.draw(screen)
         pygame.display.flip()
 
     print("Starting Asteroids!")
