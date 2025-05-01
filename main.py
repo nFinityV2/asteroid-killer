c4663c55d20ccba120569d420f488a564ce860ea
nFinityV2 <nFinitySoh@hotmail.com>
2025-05-01 14:03:10 +1000

    Refactored codes and imports

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import pygame 
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, PLAYER_RADIUS)
-    
     clock = pygame.time.Clock()
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    
     dt = 0
     
-    while screen:
-        
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+            
+        updateable.update(dt)
         pygame.Surface.fill(screen, "black")
-        dt = clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import *
 import pygame
 
 class Player(CircleShape):
