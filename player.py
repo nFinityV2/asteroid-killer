2f5ac9dcc247d11d3cd44da627bc2093b2371b94
nFinityV2 <nFinitySoh@hotmail.com>
2025-06-11 22:50:52 +1000

    Limited rate of shots

--- player.py
+++ player.py
@@ -4,9 +4,11 @@ from shot import Shot
 import pygame
 
 class Player(CircleShape):
+    
     def __init__(self, x, y, radius):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.rate_timer = 0
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,6 +26,7 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.rate_timer -= dt
         
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -41,6 +44,9 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
         
     def shoot(self):
+        if self.rate_timer > 0:
+            return
+        self.rate_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         
