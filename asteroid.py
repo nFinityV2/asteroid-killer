027d7402bb28eee9307aed5cfe325bc08a67976e
nFinityV2 <nFinitySoh@hotmail.com>
2025-06-12 19:59:25 +1000

    Refactored code, asteroids no longer split linearly, and have a higher velocity

--- asteroid.py
+++ asteroid.py
@@ -14,13 +14,16 @@ class Asteroid(CircleShape):
     
     def split(self):
         self.kill()
-        if self.radius <= 2:
+        if self.radius <= ASTEROID_MIN_RADIUS:
             return
         else:
             random_angle = random.uniform(20,50)
-            random_velocity = self.velocity.rotate(random_angle) * 1.2
+            random_velocity_a = self.velocity.rotate(random_angle) 
+            random_velocity_b = self.velocity.rotate(-random_angle) 
+            
             new_radius = self.radius - ASTEROID_MIN_RADIUS
+            
             ast_one = Asteroid(self.position.x, self.position.y, new_radius)
-            ast_one.velocity = random_velocity
+            ast_one.velocity = random_velocity_a * 1.5
             ast_two = Asteroid(self.position.x, self.position.y, new_radius)
-            ast_two.velocity = -random_velocity
+            ast_two.velocity = random_velocity_b * 1.8
